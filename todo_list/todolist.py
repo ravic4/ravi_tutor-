30086377c9bd664efd41c60c885653d36477e2fb
ravic4 <ravi220299@gmail.com>
2023-12-30 14:03:07 -0800

    update_#1: Variables initialized

--- todo_list/todolist.py
+++ todo_list/todolist.py
@@ -1,1 +1,14 @@
-print("helloworld")
+"""
+ToDo List 
+"""
+
+"""
+1: Creating my first dictionary containing my title + description
+"""
+#1) Initialize my variables: 
+title = str(input("Enter the title of the ToDo list: ")) 
+description = str(input("Describe your input: "))
+counter = 0 
+
+#2) Create my dictionary 
+
